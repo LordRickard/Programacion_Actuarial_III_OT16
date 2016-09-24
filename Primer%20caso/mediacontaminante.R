2b4c450916f6a2a934157d3d3c196ee3d7983e38
Arturo Ramírez <arturo.ramirez13@gmail.com>
2016-09-24 17:03:33 -0500

    Primer caso

---
+++ Primer%20caso/mediacontaminante.R
@@ -0,0 +1,24 @@
+
+mediacontaminante <- function(directorio, contaminante, id = 1:332){
+  suma <- numeric()
+  for (c in id){
+    
+    id1 <- formatC(c,width = 3 ,flag = "0")
+    readen <- read.csv(paste(id1, ".csv",sep=""),header = T)
+    
+    if (contaminante == "sulfate"){
+      
+      #suma <- c(suma,readen[[sulfate]])
+      suma <- c(suma,readen$sulfate)
+    } else if (contaminante == "nitrate"){
+      
+      #suma <- c(suma,readen[[nitrate]])
+      suma <- c(suma,readen$nitrate)
+    } else {
+      paste("El contaminante",contaminante,"no existe.")
+    } 
+    
+  }
+  promedio <- mean(suma, na.rm = T)
+  promedio
+}
